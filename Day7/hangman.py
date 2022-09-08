507a08aeb99924bc806b94c7497314b07f5a8910
Aslam Jabri <jabri.aslam@gmail.com>
2022-09-08 13:21:42 +0530

    Day7

---
+++ Day7/hangman.py
@@ -0,0 +1,36 @@
+import random
+from hangman_art import *
+from hangman_alph import *
+lives = 6
+print(logo)
+chosen = random.choice(word_list)
+length_words = len(chosen)
+display =[]
+for _ in chosen:
+    display += "_"
+print(display)
+ 
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess the letter:").lower()
+    if guess in display:
+        print(f"You have already guessed {guess}.")
+
+    for position in range(length_words):
+        letter = chosen[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+    if guess  not in chosen:
+        print(f"You have guessed {guess} letter , you lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")  
+    print(stages[lives])
+    
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win.")
+    
